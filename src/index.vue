1167746fbea3a5fd160c119f59565d03918a7aef
coderC <2448856797@qq.com>
2021-12-06 22:09:06 +0800

    tips

--- src/index.vue
+++ src/index.vue
@@ -3,6 +3,7 @@
     <cc-button @click="onClick" :isLoading="isLoading" value="test"></cc-button>
     <CcCard :icon="icon"></CcCard>
     <CcInput :leftIcon="icon" :rightIcon="icon" />
+    <CcTips tips="this is a tips"></CcTips>
   </div>
 </template>
 
@@ -11,10 +12,11 @@ import { defineComponent, ref } from 'vue'
 import CcButton from './packages/button/index.tsx'
 import CcCard from './packages/card/index'
 import CcInput from './packages/input/index'
+import CcTips from './packages/tips/index'
 import icon from './assets/logo.png'
 
 export default defineComponent({
-  components: {CcButton, CcCard, CcInput},
+  components: {CcButton, CcCard, CcInput, CcTips},
   setup() {
     const isLoading = ref(false)
     const onClick = () => {
--- src/packages/tips/index.tsx
+++ src/packages/tips/index.tsx
@@ -1,20 +1,47 @@
 import {
-  defineComponent, mergeProps
+  defineComponent,
+  computed,
+  ref,
+  CSSProperties
 } from 'vue'
 import './index.scss';
 
 const CcTips = defineComponent({
   name: 'cc-tips',
   props: {
-
-  },
-  setup(props, { slots, emit }) {
-
-    return () => {
-      <div class="cc-tips">
-        
-      </div>
+    tips: {
+      type: String
     }
+  },
+  emits: ['hover', 'hoverout'],
+  setup(props, { slots, emit }) {
+    const isTips = ref<boolean>(false)
+    const opacity = ref<number>(0)
+    const onHover = (e: Event) => {
+      isTips.value = true
+      emit('hover', e)
+    }
+    const onHoverOut = (e: Event) => {
+      isTips.value = false
+      emit('hoverout', e)
+    }
+
+    const styles = computed(() => {
+      if (isTips.value) return;
+      const style: CSSProperties & {[propname: string]: any} = {
+        '--animation': 'hidetip 3s 1',
+        'opacity': opacity.value
+      }
+      return style
+    })
+
+    return () => (
+      <div class="cc-tips" onMouseover={onHover} onMouseout={onHoverOut}>
+        { props.tips }
+        { <div class="hover-tips" style={styles.value}>{ props.tips }</div> }
+        { slots.default?.() }
+      </div>
+    )
   }
 })
 
