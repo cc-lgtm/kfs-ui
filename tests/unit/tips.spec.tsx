3f0f72669cc085e141a9022d2434900efd85504d
coderC <2448856797@qq.com>
2021-12-28 22:04:07 +0800

    test(tips): props & hover & tips

---
+++ tests/unit/tips.spec.tsx
@@ -0,0 +1,33 @@
+import {ref} from 'vue'
+import { mount } from '@vue/test-utils';
+import Tips from './../../packages/tips/index'
+
+const testTips = {
+  template: `<Tips :tips="tips" @hover="onHover">{{msg}}</Tips>`,
+  props: ['tips'],
+  setup() {
+    const msg = ref('this is a tips')
+    const onHover = () => {
+      console.log('111')
+      msg.value = 'this not is a tips'
+    }
+    return {
+      msg,
+      onHover
+    }
+  }
+}
+test('disabled text', async () => {
+  const wrapper = mount(testTips, {
+    props: {
+      msg: 'this is a tips'
+    }
+  })
+  expect(wrapper.attributes('msg')).toContain('this is a tips')
+  expect(wrapper.text()).toContain('this is a tips')
+  await wrapper.setProps({'class': 'myClass'})
+  expect(wrapper.attributes('class')).toContain('myClass')
+  await wrapper.trigger('hover')
+  expect(wrapper.text()).toContain('this not is a tips')
+})
+
