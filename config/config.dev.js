408fe7d1b5b26f4af65517cf3158daf27c9a97c6
coderC <2448856797@qq.com>
2021-12-28 15:03:09 +0800

    build(packages): pack on demand

---
+++ config/config.dev.js
@@ -0,0 +1,21 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('ts')
+      .include
+      .add('/packages')
+      .end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap(options => {
+        return options
+      })
+  }
+}
