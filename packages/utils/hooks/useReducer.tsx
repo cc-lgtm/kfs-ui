84d97086319ece9dcfb1a3c2a1559c8209172952
coderC <2448856797@qq.com>
2021-12-30 09:24:26 +0800

    fix(useReducer): init

--- packages/utils/hooks/useReducer.tsx
+++ packages/utils/hooks/useReducer.tsx
@@ -12,33 +12,8 @@ const useReducer = (reducer: ReducerType, initialArg: any, init?: InitType) => {
       state[action] = newVal[action]
       reducer(initialArg, action)
     })
-    return newVal
   }
   return [readonly(state), dispatch]
-}
-
-const initialState = {count: 0};
-
-function reducer(state: {[propname: string]: number}, action: {[propname: string]: string}) {
-  switch (action.type) {
-    case 'increment':
-      return {count: state.count + 1};
-    case 'decrement':
-      return {count: state.count - 1};
-    default:
-      throw new Error();
-  }
-}
-
-function Counter() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <>
-      Count: {state.count}
-      <button onClick={() => dispatch({'type': 'decrement'})}>-</button>
-      <button onClick={() => dispatch({'type': 'increment'})}>+</button>
-    </>
-  );
 }
 
 export default useReducer;
