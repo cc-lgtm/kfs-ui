f5e5b9579c3535ba9e32224a46c05390d3cef82f
coderC <2448856797@qq.com>
2021-12-30 10:19:44 +0800

    update(hooks): useClass

---
+++ packages/utils/hooks/useClass.ts
@@ -0,0 +1,16 @@
+import { computed } from 'vue'
+import {tuple} from '../helper/index';
+
+type ClassType = {
+  state: boolean,
+  class: string
+}
+const useClass = computed((initClass?: string) => {
+  const classes = tuple<string>([initClass])
+  const addClass = (source: ClassType): void => {
+    source.state && classes.push(source.class)
+  }
+  return [classes.join(' '), addClass] as [string, (source: ClassType) => void]
+})
+
+export default useClass;
