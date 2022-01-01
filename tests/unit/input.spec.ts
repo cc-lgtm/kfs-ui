e8cd5dc0e9d46749abc8e2b5aa10d40be2de1194
coderC <2448856797@qq.com>
2022-01-01 10:08:15 +0800

    test(input) value

---
+++ tests/unit/input.spec.ts
@@ -0,0 +1,21 @@
+import { mount } from '@vue/test-utils'
+import Input from '../../packages/input/index'
+
+const testInput = {
+  template: '<Input id="input" value="test data" @input="onInput" />',
+  setup() {
+    const onInput = (value: string) => {
+      console.log(value)
+    }
+    return {
+      onInput
+    }
+  }
+}
+
+test('test input value', async () => {
+  const wrapper = mount(testInput)
+  const Input = wrapper.find('#input')
+  await Input.setValue('test data')
+  // expect(Input.element.value).toBe('test data')
+})
--- tests/unit/input.spec.tsx
+++
@@ -1,6 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Input from './../../packages/input/index'
-
-const testInput = {
-  template: '<Input></Input>'
-}
