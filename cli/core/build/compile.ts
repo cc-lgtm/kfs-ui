3f87f3bb7c68867c949254d788672a1575d197bf
coderC <2448856797@qq.com>
2022-01-03 10:35:52 +0800

    fix(cli) build

--- cli/core/build/compile.ts
+++ cli/core/build/compile.ts
@@ -7,18 +7,22 @@ const buildAll = async (entry: string, output: string) => {
     configureWebpack: {
       entry: ${JSON.stringify(entry)},
       output: {
-        filename: '[name]/index.js'
+        filename: '[name]/index.js',
+        libraryExport: 'default'
       }
+    },
+    filenameHashing: false,
+    css: {
+      extract: false,
+      sourceMap: false,
+      requireModuleExtension: false
     }
   }`
   const packages = fs.readdirSync(entry)
   fs.mkdir(output, () => {
     packages.forEach((component, _) => {
       const name = (component as string)
-      if (name === 'utils') return;
-      if (name === 'index.ts' || name === 'message') {
-        return;
-      }
+      if (name === 'utils' || name === 'index.ts') return;
       ENTRY[component] = `./packages/${component}/index.ts`
     })
     fs.writeFileSync(`${output}/index.js`, configContent(ENTRY))
--- config/index.js
+++ config/index.js
@@ -1,37 +1,16 @@
 module.exports = {
-    outputDir: './lib3',
+    outputDir: './lib',
     configureWebpack: {
-      entry: {
-        index: './packages/index.ts'
-        // button: './packages/button/index.ts',
-        // card: './packages/card/index.ts',
-        // datepicker: './packages/datepicker/index.ts',
-        // drawer: './packages/drawer/index.ts',
-        // input: './packages/input/index.ts',
-        // link: './packages/link/index.ts',
-        // loading: './packages/loading/index.ts',
-
-        // message: './packages/message/index.ts',
-        // option: './packages/option/index.ts',
-        // pagination: './packages/pagination/index.ts',
-        // popup: './packages/popup/index.ts',
-        // radio: './packages/radio/index.ts',
-
-        // radioGroup: './packages/radioGroup/index.ts',
-        // rate: './packages/rate/index.ts',
-        // select: './packages/select/index.ts',
-        // skeleton: './packages/skeleton/index.ts',
-        // 'skeleton-item': './packages/skeleton-item/index.ts',
-        // stepbar: './packages/stepbar/index.ts',
-
-        // stepitem: './packages/stepitem/index.ts',
-        // switch: './packages/switch/index.ts',
-        // tabs: './packages/tabs/index.ts',
-        // tab: './packages/tab/index.ts',
-        // tips: './packages/tips/index.ts'
-      },
+      entry: {"button":"./packages/button/index.ts","card":"./packages/card/index.ts","datepicker":"./packages/datepicker/index.ts","drawer":"./packages/drawer/index.ts","input":"./packages/input/index.ts","link":"./packages/link/index.ts","loading":"./packages/loading/index.ts","message":"./packages/message/index.ts","option":"./packages/option/index.ts","pagination":"./packages/pagination/index.ts","popup":"./packages/popup/index.ts","radio":"./packages/radio/index.ts","radioGroup":"./packages/radioGroup/index.ts","rate":"./packages/rate/index.ts","select":"./packages/select/index.ts","skeleton":"./packages/skeleton/index.ts","skeleton-item":"./packages/skeleton-item/index.ts","stepbar":"./packages/stepbar/index.ts","stepitem":"./packages/stepitem/index.ts","switch":"./packages/switch/index.ts","tab":"./packages/tab/index.ts","tabs":"./packages/tabs/index.ts","tips":"./packages/tips/index.ts"},
       output: {
-        filename: '[name]/index.js'
+        filename: '[name]/index.js',
+        libraryExport: 'default'
       }
+    },
+    filenameHashing: false,
+    css: {
+      extract: false,
+      sourceMap: false,
+      requireModuleExtension: false
     }
   }
