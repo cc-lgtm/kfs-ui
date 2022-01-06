d1ff0d3b976556f93aed908abd31c554460bd3e0
coderC <2448856797@qq.com>
2022-01-06 17:47:06 +0800

    md(README) add todo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### todo
 cli 自动按需导入  
-组件库换肤 & 服务端渲染组件库
+组件库自定义主题(已实现useTheme)  
+服务端渲染组件库  
+修改组件内部逻辑(useContext & teleport)
--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <cc-button type="success">按钮</cc-button>
     <cc-button type="error">按钮</cc-button>
     <cc-button type="warn">按钮</cc-button>
+    <cc-card shadow="never" />
   </div>
 </template>
 
